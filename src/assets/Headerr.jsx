408d860f71bbd6aa0190043b88567dea939b0773
manuj <manujdixit04@gmail.com>
2023-12-06 21:29:47 +0530

    q

--- src/assets/Headerr.jsx
+++ src/assets/Headerr.jsx
@@ -3,7 +3,9 @@ import { useNavigate } from "react-router-dom";
 
 const Header = () => {
   const navigate = useNavigate();
-
+  const gotonotes = () => {
+    navigate("/mynotes");
+  };
   return (
     <div>
       <header>
@@ -24,8 +26,8 @@ const Header = () => {
             </a>
             <div className="flex items-center lg:order-2">
               <a
-                href="/mynotes"
                 className="inline-block bg-gray-800 dark:bg-white text-white dark:text-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2"
+                onClick={{ gotonotes }}
               >
                 My notes
               </a>
